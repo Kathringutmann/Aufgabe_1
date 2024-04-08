1e1f1dc18648631fe38c285e56b37b83d3c81ffc
Kathringutmann <gk2575@mci4me.at>
2024-04-08 16:13:36 +0200

    code funktioniert

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello world")
+
+
+import matplotlib.pyplot as plt
+
+# Laden der Daten
+from load_data import load_data
+from sort import bubble_sort
+
+power = load_data("activity.csv")
+print(power)
+power = power["PowerOriginal"]
+sorted_power_W = bubble_sort(power)
+#print(sorted_power_W)
+# Plotte mir eine Liste mit Matplotlib
+plt.plot(sorted_power_W[::-1])
+plt.xlabel('Dauerlinie')
+plt.ylabel('P in W')
+plt.savefig("figures\power_curve3.png")
+
+
+#save_figure('power_curve.png')
+#plt.show()
